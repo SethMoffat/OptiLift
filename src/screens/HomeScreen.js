43f2001a98c417f7c850abb9882d9a42a2d1a441
Seth Moffat <sethmoffat1@gmail.com>
2024-03-08 13:27:10 -0700

    reverting time

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -11,12 +11,8 @@ const HomeScreen = ({ darkMode }) => {
     <div className={`HomeScreen ${darkMode ? 'dark-mode' : ''}`}>
       <div style={{ textAlign: 'center' }}>
         <h1>Opti-Lift</h1>
-        <Link to="/accounts" style={{ textDecoration: 'none' }}>
-          <button style={{ padding: '20px', fontSize: '20px', margin: '10px' }}>Google business accounts</button>
-        </Link>
-        <Link to="/new-client" style={{ textDecoration: 'none' }}> {/* Add Link to NewClientScreen */}
-          <button style={{ padding: '20px', fontSize: '20px', margin: '10px' }}>Add Client</button>
-        </Link>
+        <button style={{ padding: '20px', fontSize: '20px', margin: '10px' }}>Google business accounts</button>
+        <button style={{ padding: '20px', fontSize: '20px', margin: '10px' }}>Add Client</button>
         <Link to="/settings" style={{ textDecoration: 'none' }}>
           <button style={{ padding: '20px', fontSize: '20px', margin: '10px' }}>Settings</button>
         </Link>
